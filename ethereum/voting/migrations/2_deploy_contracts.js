09f4f0c763694a50641adcd5ca519498ac1816b1
lizorn <lizorn@fosmid.cn>
2018-05-22 13:13:41 +0800

    update voting version2

--- ethereum/voting/migrations/2_deploy_contracts.js
+++ ethereum/voting/migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-var VotingContract = artifacts.require("./Voting.sol");
+var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(VotingContract,['Rama', 'Nick', 'Jose'], {gas: 6700000});
+  // deployer.deploy(VotingContract,['Rama', 'Nick', 'Jose'], {gas: 6700000});
+  deployer.deploy(Voting, 1000, web3.toWei('0.1', 'ether'), ['Rama', 'Nick', 'Jose']);
 };
