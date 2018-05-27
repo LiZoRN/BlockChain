bc24f01d0780e9081a6bf128b7d9ed4e7812951e
lizorn <lizorn.l@gmail.com>
2018-05-27 16:02:03 +0800

    add ethereumpy

---
+++ ethereumpy/api/serializers.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'lizorn'
+__date__ = '2018/5/27 14:42'
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
---
+++ ethereumpy/polls/urls.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'lizorn'
+__date__ = '2018/5/27 11:14'
+
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
